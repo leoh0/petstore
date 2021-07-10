0753b6f166098e785a7a1402f87b3ec40e900378
Eohyung Lee <liquidnuker@gmail.com>
2021-07-10 23:11:08 +0900

    Readable image name

---
+++ hack/print-workspace-status.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+git_commit="$(git describe --tags --always --dirty)"
+build_date="$(date -u '+%Y%m%d')"
+docker_tag="v${build_date}-${git_commit}"
+
+cat <<EOF
+DOCKER_TAG ${docker_tag}
+EOF
